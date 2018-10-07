562abcfe6161ef78b4b5a3fcb2220b3f5e7c5e6c
DaveParillo <DaveParillo@users.noreply.github.com>
2018-10-06 21:14:53 -0700

    removed virtual and overide and fixed typos in mape

--- sources/sword.cpp
+++ sources/sword.cpp
@@ -2,12 +2,12 @@
 items::sword::sword(){
 	name = "Default Sword";
 	stats = {
-		{"STR" : 3},
-		{"DEX" : 1},
-		{"INT" : 1}
-	}
+		{"STR", 3},
+		{"DEX", 1},
+		{"INT", 1}
+	};
 }
-virtual int items::sword::calculate_damage(std::map<std::string, int> character_stats) overide {
+int items::sword::calculate_damage(stats_t character_stats) {
 	std::cout << "Sword damage uses strength";
 	return stats["STR"] + character_stats["STR"];
 }
